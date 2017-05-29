c406a3de8c73352d531d59cc8318d020c1f9b702
webwerks <bimal.sharma@neosofttech.com>
2017-05-29 12:39:59 +0530

    changes in edit option

--- Modules/Questionnaire/Resources/views/admin/questionnaires/partials/edit-fields.blade.php
+++ Modules/Questionnaire/Resources/views/admin/questionnaires/partials/edit-fields.blade.php
@@ -2,7 +2,7 @@
     <p>
         <div class="box-body">
 	    {!! Form::text("question", old("{$questionnaire}.question", "{$questionnaire['question']}"), ['class' => 'form-control', 'data-slug' => 'source', 'placeholder' => 'Question']) !!}
-	    <textarea class="ckeditor" name="{{$lang}}[body]" rows="10" cols="80">
+	    <textarea class="ckeditor" name="answer" rows="10" cols="80">
                 {!! old("$questionnaire.answer", "{$questionnaire['answer']}") !!}
             </textarea>
 	</div>
